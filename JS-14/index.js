64f097b60c6174086748b0715c4ea7f01736b49d
rohit jain <rohitjain97130@gmail.com>
2025-02-12 15:30:22 +0530

    done

--- JS-14/index.js
+++ JS-14/index.js
@@ -3,11 +3,11 @@ class Car{
         this.model = model;
         this.color = color;
         this.getdetails = function(){
-            return `Car model is ${this.model} and color is ${this.color}`;
+            return `Carrr model is ${this.model} and color is ${this.color}`;
         }
     }
 }
 
 class SuperCar {
-    
+
 }
